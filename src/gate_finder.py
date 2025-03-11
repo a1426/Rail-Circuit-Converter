207c0d4fc065ab5eb3133310411c19424a49e622
a1426 <66330789+a1426@users.noreply.github.com>
2025-03-11 18:29:43 -0500

    Update gate_finder.py

--- src/gate_finder.py
+++ src/gate_finder.py
@@ -5,7 +5,7 @@ def single_square_gates(img):
     width, height = im.size
     #Forces RGB rather than RGBA
     im=im.convert("RGB")
-    #A rather crude approach, we look for the non-greyscale colors in the image.
+    #A rather crude aproach, we look for the non-greyscale colors in the image.
     #We must use a .png file, as artifacts can throw this approach off.
     min_y,max_y=height,0
     previously_colored=False
@@ -28,3 +28,32 @@ def single_square_gates(img):
             x_ranges[-1].append(x)
         previously_colored=colored_column
     return (x_ranges,min_y,max_y,width,height)
+
+def measure_gates(img):
+    im=Image.open(img)
+    width, height = im.size
+    #Forces RGB rather than RGBA
+    im=im.convert("RGB")
+    #Here, we check for the rightmost occurence of 168,168,168, the color of the measurement gate, since we know it is at the end.
+    detected=False
+    for x in range(width-1,0,-1):
+        for y in range(height):
+            r,g,b=im.getpixel((x,y))
+            if(r==g==b==168):
+                max_x=min_x=x
+                min_y=max_y=y
+                detected=True
+                break
+        if detected:
+            break
+    while(True):
+        r,g,b=im.getpixel((min_x,min_y))
+        if(r==g==b==168):
+            min_x-=1
+        else: break
+    while(True):
+        r,g,b=im.getpixel((max_x,max_y))
+        if(r==g==b==168):
+            max_y+=1
+        else: break        
+    return (min_x,max_x,min_y,max_y,width,height)
